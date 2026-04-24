c9c0e400b940debfa6a9a11a91fc35a8a2fa6a4e
satakshi003 <soumya3955@gmail.com>
2026-04-24 22:20:05 +0530

    Set up database connection and user routes in server initialization

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import cors from "cors";
 import "dotenv/config";
+import connectDB from './config/db.js';
+
 
 const app = express();
 const PORT =  process.env.PORT || 3000;
+
+await connectDB();
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => res.send("Server is live..."))
+
+import userRouter from "../server/Routes/userRoutes.js"
+app.use('/api/users', userRouter)
+
 
 app.listen(PORT, ()=> {
   console.log(`Server is running on port ${PORT}`);
