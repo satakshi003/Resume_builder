9763398e67f15e7d4bbe569e7c9f64062a180903
satakshi003 <soumya3955@gmail.com>
2026-04-26 20:20:19 +0530

    Add Resume model schema for user resumes

---
+++ server/models/Resume.js
@@ -0,0 +1,81 @@
+import mongoose, {Schema} from "mongoose";
+
+const ResumeSchema = new Schema(
+  {
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      title: {
+        type: String,
+        default: "Untitled Resume"
+        
+      },
+ 
+    public: {
+      type: Boolean,
+      default: false
+    },
+    template: {
+      type: String,
+      default: "classic"
+    },
+    template: {
+      type: String,
+      default: "#3B82F6"
+    },
+    professional_summary: {
+      type: String,
+      default: ""
+    },
+    skills: [
+      {
+        type: String
+      }
+    ],
+    personal_info: { //For ONE resume → only ONE person so not in array.
+      image: {type: String, default: ''},
+      full_name: {type: String, default: ''},
+      profession: {type: String, default: ''},
+      email: {type: String, default: ''},
+      phone: {type: String, default: ''},
+      location: {type: String, default: ''},
+      linkedin: {type: String, default: ''},
+      website: {type: String, default: ''},
+
+    },
+    experience: [
+      {
+        company: {type: String},
+        position: {type: String},
+        start_date: {type: String},
+        end_date: {type: String},
+        description: {type: String},
+        is_current: {type: String},
+      }
+    ],
+    project: [
+      {
+        name: {type: String},
+        type: {type: String},
+        description: {type: String},
+      }
+    ],
+    education: [
+      {
+        institution: {type: String},
+        degree: {type: String},
+        field: {type: String},
+        graduation_date: {type: String},
+        gpa: {type: String},
+      }
+    ],
+  },
+  {
+    timestamps: true,
+    minimize: false
+  }
+  
+)
+
+export  const Resume = mongoose.model("Resume", ResumeSchema);
