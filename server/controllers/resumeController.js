4696aef4a86bf6eb30e9d3eb2b0209239755df7c
satakshi003 <soumya3955@gmail.com>
2026-04-29 23:51:36 +0530

    Add image upload functionality and update resume handling

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -7,6 +7,8 @@ import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import generateAccessAndRefreshTokens from "../helpers/generateAccessAndRefreshTokens.js";
 import { Resume } from "../models/Resume.js";
+import imagekit from "../config/imagekit.js";
+import fs from 'fs';
 
 const createResume = async (req, res) => {
   try{
@@ -74,5 +76,57 @@ const getResumeById = async (req, res) => {
   }
 }
 
+const getPublicResumeId = async (req, res) => {
+  try{
+    const {resumeId} = req.params;
+    const resume = await Resume.findOne({public: true, _id: resumeId})
 
+    if(!resume){
+      return res.status(404).json({message: "Resume not found"})
+    }
+    return res.status(200).json({resume})
+  }catch(error){
+    return res.status(400).json({message: error.message})
+  }
+}
 
+const updateResume = async (req, res) => {
+  try{
+     const userId = req.userId;
+     const {resumeId, resumeData, removeBackground} = req.body
+     const image = req.file;
+
+     let resumeDataCopy = JSON.parse(resumeData); //Backend receives JSON as string → needs parsing
+
+     if(image){
+
+      const imageBufferData = fs.createReadStream(image.path)
+      
+        const response = await imagekit.files.upload({
+          file: imageBufferData,
+          fileName: "resume.png",
+          folder: "user-resumes",
+          transformation: {
+            pre: 'w-300, h-300, fo-face, z-0.75' + (removeBackground ? 'e-bgremove': '')
+          }
+        });
+
+        resumeDataCopy.personal_info.image = response.url
+     }
+
+     const resume = await Resume.findByIdAndUpdate({userId, _id: resumeId}, resumeDataCopy, {new: true})
+
+     return res.status(200).json({message: 'Saved successfully', resume})
+  }
+  catch(error){
+    return res.status(400).json({message: error.message})
+  }
+}
+
+export default {
+  createResume,
+  deleteResume,
+  getResumeById,
+  getPublicResumeId,
+  updateResume
+}
