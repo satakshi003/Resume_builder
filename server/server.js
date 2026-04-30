47d858118f4163deb544146375f6e186b9ae1659
satakshi003 <soumya3955@gmail.com>
2026-04-30 22:17:21 +0530

    feat: add missing imports and fix API route for AI integration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cors from "cors";
 import "dotenv/config";
 import connectDB from './config/db.js';
-
+import userRouter from "../server/Routes/userRoutes.js"
+import resumeRouter from "../server/Routes/resumeRoutes.js"
+import aiRouter from "../server/Routes/aiRoutes.js"
 
 const app = express();
 const PORT =  process.env.PORT || 3000;
@@ -14,8 +16,11 @@ app.use(cors())
 
 app.get('/', (req, res) => res.send("Server is live..."))
 
-import userRouter from "../server/Routes/userRoutes.js"
+
 app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
+app.use('api/ai', aiRouter)
+
 
 
 app.listen(PORT, ()=> {
