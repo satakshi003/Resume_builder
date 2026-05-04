072a71a28c96ae6a6cbbfde25d8802e2a2346604
satakshi003 <soumya3955@gmail.com>
2026-05-04 23:11:42 +0530

    feat: implement loading state and user authentication in Layout component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import {useSelector} from 'react-redux'
+import Login from './Login'
+import { Loader } from '../components/Loader'
 
 const Layout = () => {
+
+  const {user, loading} = useSelector(state => state.auth)
+
+  if(loading){
+    return <Loader />
+  }
+
   return (
     <div>
-      <div className='min-h-screen bg-gray-50'>
+      {
+        user ? (
+            <div className='min-h-screen bg-gray-50'>
         <Navbar />
         <Outlet />
       </div>
+        )
+        : <Login />
+      }
+      
     </div>
   )
 }
